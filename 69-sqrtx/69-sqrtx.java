c39e25e48527599837dcf817a7309cedc97b3e04
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2023-06-03 20:43:36 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (30.82%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -3,13 +3,13 @@ class Solution {
         int l = 1, r= x ;
         while(l <= r){
             int mid = l + ( r - l) / 2;
-            if(Math.pow(mid,2) == x)
+            if(Math.pow(mid,2) == x)  // or mid = x / mid  => mid * mid = x
                 return mid;
             else if ( Math.pow(mid,2) > x )
                 r = mid - 1;
             else
                 l = mid + 1;
         }
-        return l - 1;
+        return l  - 1;
     }
 }
