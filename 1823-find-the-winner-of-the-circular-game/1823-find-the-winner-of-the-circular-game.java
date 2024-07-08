b9f2d1035bf6809aa302e0cbeb627806a7fdeae2
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2024-07-08 12:04:20 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (82.80%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,11 +1,19 @@
 class Solution {
+    // public int findTheWinner(int n, int k) {
+    //     int ans = solve(n, k) + 1;
+    //     return ans;
+    // }
+    // public int solve(int n, int k){
+    //     if(n == 1)
+    //         return 0;
+    //     return (solve(n -1, k) + k) % n;
+    // }
+    
     public int findTheWinner(int n, int k) {
-        int ans = solve(n, k) + 1;
-        return ans;
-    }
-    public int solve(int n, int k){
-        if(n == 1)
-            return 0;
-        return (solve(n -1, k) + k) % n;
+        int winner=0;
+        for (int i = 1; i <= n; i++) {
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
     }
 }
